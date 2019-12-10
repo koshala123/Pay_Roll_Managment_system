c1abb374f95ebf43ad4f0d2af996eda4b193ad4b
koshala123 <51772480+koshala123@users.noreply.github.com>
2019-12-10 16:51:51 +0530

    Adding OverTime Database

---
+++ Pay_Roll_Managment_System/Models/OverTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pay_Roll_Managment_System.Models
+{
+    public class OverTime
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OverTimeId { get; set; }
+        public int AdditionalHours { get; set; }
+        public int Amount { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
--- Pay_Roll_Managment_System/Startup.cs
+++ Pay_Roll_Managment_System/Startup.cs
@@ -34,6 +34,7 @@ namespace Pay_Roll_Managment_System
             ) ;
             services.AddScoped<IEmployeeRepository,EmployeeRepository>();
             services.AddScoped<IPositionRepository, PositionRepository>();
+            services.AddScoped<IAttendanceRepository, AttendanceReposiroty>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
