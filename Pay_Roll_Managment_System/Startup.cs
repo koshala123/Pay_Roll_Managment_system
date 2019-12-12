476e9d743db05049e660ad29fed0f20a10052956
Chamara Madushanka <chamarakv98madushanka@gmail.com>
2019-12-12 19:08:45 +0530

    aaron changes

--- Pay_Roll_Managment_System/Startup.cs
+++ Pay_Roll_Managment_System/Startup.cs
@@ -62,8 +62,6 @@ namespace Pay_Roll_Managment_System
 
             app.UseCors("AllowMyOrigin");
 
-            app.UseCors(options => options.WithOrigins("http://localhost:3000").AllowAnyMethod());
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
