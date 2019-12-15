cfe703fb6a5be1e46c1578f19e926277e9ef252f
Aaron <aaronfernando.edu@hotmail.com>
2019-12-15 20:25:56 +0530

    Adding buisness logic to overtime dto class

--- Pay_Roll_Managment_System/Dtos/OverTimeDto.cs
+++ Pay_Roll_Managment_System/Dtos/OverTimeDto.cs
@@ -8,10 +8,12 @@ namespace Pay_Roll_Managment_System.Dtos
     public class OverTimeDto
     {
         public int OverTimeId { get; set; }
-        public int AdditionalHours { get; set; }
+        public int WorkHour { get; set; }
         public int Amount { get; set; }
         public string EmployeeFirstName { get; set; }
         public string EmployeeLastName { get; set; }
         public int EmployeeId { get; set; }
+
+        
     }
 }
